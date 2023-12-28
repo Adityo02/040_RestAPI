9cd91122d8ae74e6f35f5d84efbc56322b400162
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2023-12-28 10:55:28 +0700

    Menambhakan InsertViewModel.kt

--- app/src/main/java/com/example/datainternet/repository/KontakRepository.kt
+++ app/src/main/java/com/example/datainternet/repository/KontakRepository.kt
@@ -11,5 +11,21 @@ interface KontakRepository {
 class NetworkKontakRepository(
     private val kontakApiService: KontakService
 ) : KontakRepository {
+
     override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+    override suspend fun deleteKontak(id: Int): Boolean {
+        // Implementasi penghapusan kontak melalui kontakApiService dengan menggunakan id
+        val response = kontakApiService.deleteKontak(id)
+
+        // Mengembalikan nilai true jika penghapusan berhasil, false sebaliknya
+        return response.isSuccessful
+    }
+
+    override suspend fun insertKontak(kontak: Kontak): Boolean {
+        // Implementasi penambahan kontak melalui kontakApiService
+        val response = kontakApiService.insertKontak(kontak)
+
+        // Mengembalikan nilai true jika penambahan berhasil, false sebaliknya
+        return response.isSuccessful
+    }
 }
